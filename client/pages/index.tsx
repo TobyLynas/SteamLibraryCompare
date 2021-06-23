94cbcff68a07162686b5030f1a948d4c30c09c26
hensm <m@matt.tf>
2021-06-23 17:41:18 +0100

    Replace existing client with next

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+const HomePage = () => (
+    <div>
+        <h1>Hello, world!</h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt
+            possimus praesentium sunt, doloremque sit nostrum aperiam porro iste
+            omnis cupiditate, consectetur error explicabo soluta consequuntur
+            assumenda odit ratione? Similique, eveniet!
+        </p>
+    </div>
+);
+
+export default HomePage;
