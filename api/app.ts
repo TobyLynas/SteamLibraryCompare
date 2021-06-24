944a58c5bde8803fbc4afce5e1d828d30544708e
hensm <m@matt.tf>
2021-06-24 05:44:15 +0100

    Add CORS middleware

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const PORT = 9000;
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello, world!");
