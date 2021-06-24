5f70cc7d3e2df92720ca4fef3e68804613012cd5
Hannah-Frost <h.7frost@gmail.com>
2021-06-24 14:10:38 +0100

    Add top nav + styling

---
+++ client/components/TopNav.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/topNav.module.css";
+
+const TopNav = () => (
+    <div className={styles.topNav}>
+        <p className={styles.heading}>Steam Library</p>
+    </div>
+);
+
+export default TopNav;
