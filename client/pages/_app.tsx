136ff575dc7481c1e2333afb006121854bc8e25e
Hannah-Frost <h.7frost@gmail.com>
2021-06-25 10:46:54 +0100

    Update modules to use global theme

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,18 +1,10 @@
 import React from "react";
+import "../styles/global.css";
 import PageLayout from "../components/PageLayout";
 
 function MyApp({ Component, pageProps }) {
     return (
         <PageLayout>
-            <style jsx global>{`
-                body {
-                    margin: 0;
-                    font-family: inter, Sans-serif;
-                }
-                p {
-                    margin: 0;
-                }
-            `}</style>
             <Component {...pageProps} />
         </PageLayout>
     );
