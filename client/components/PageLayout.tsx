026c60a08455ba96d0f42135da172c4352db3e40
Hannah-Frost <h.7frost@gmail.com>
2021-07-09 16:48:57 +0100

    Set typing for next props + layout

--- client/components/PageLayout.tsx
+++ client/components/PageLayout.tsx
@@ -3,10 +3,10 @@ import Header from "./Header";
 import Footer from "./Footer";
 import styles from "../styles/pageLayout.module.css";
 
-const PageLayout = props => (
+const PageLayout = ({ children }: { children: React.ReactNode }) => (
     <div className={styles.layout}>
         <Header />
-        <div className={styles.content}>{props.children}</div>
+        <div className={styles.content}>{children}</div>
         <Footer />
     </div>
 );
