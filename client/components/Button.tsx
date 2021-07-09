9de9e0bd35a9753c04453b2d323e974e98b2f96d
Toby Lynas <tobylynas@tobys-air.lan>
2021-07-09 16:32:09 +0100

    Implemented most of splash page

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -1,7 +1,6 @@
 /* Reusable button component, use the text props to set the text for the button
 and the runFunction prop should be a function with what the button should do*/
 
-
 import styles from "/styles/button.module.css";
 
 interface ButtonProps {
@@ -11,9 +10,11 @@ interface ButtonProps {
 
 const Button = (props: ButtonProps) => {
     return (
-        <button className={styles.box} onClick={() => props.runFunction()}>
-            {props.text}
-        </button>
+        <div className={styles.container}>
+            <button className={styles.box} onClick={() => props.runFunction()}>
+                {props.text}
+            </button>
+        </div>
     );
 };
 export default Button;
--- client/components/LandingHeader.tsx
+++ client/components/LandingHeader.tsx
@@ -9,12 +9,18 @@ const joinRoom = () => {
 
 const LandingHeader = () => (
     <div className={styles.main}>
-        <div className={styles.heading}>
-            <div>Getting started</div>
-            <br />
-            <div className={styles.alignmentBox}>
-                <Button text="Host Room" runFunction={hostRoom} />
-                <Button text="Join Room" runFunction={joinRoom} />
+        <div className={styles.imgBox}>
+            <div className={styles.transparentBox}>
+                <div className={styles.grandText}>What is this?</div>
+                <div className={styles.subText}>placeholder</div>
+            </div>
+            <div className={styles.highlightBox}>
+                <div className={styles.gettingStarted}>Getting started</div>
+                <br />
+                <div className={styles.alignmentBox}>
+                    <Button text="Host Room" runFunction={hostRoom} />
+                    <Button text="Join Room" runFunction={joinRoom} />
+                </div>
             </div>
         </div>
     </div>
