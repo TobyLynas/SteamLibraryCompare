fdf9b737d184f9424a5b3fc338f4e4f55a8f842b
Toby Lynas <tobylynas@tobys-air.lan>
2021-08-10 20:56:37 +0100

    Reworked auth display system

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,19 @@
-import styles from "../styles/index.module.css";
 import LandingHeader from "../components/LandingHeader";
 import Auth from "../components/Auth";
 import React, { useState } from "react";
 
 const HomePage = () => {
     const [display, setDisplay] = useState(true);
-    const landingContainer = display ? styles.displayFlex : styles.displayNone;
-    const authContainer = display ? styles.displayNone : styles.displayFlex;
-    return (
-        <div >
-            <div className={landingContainer}>
-                <LandingHeader 
-                onClickHost={()=>setDisplay(!display)} 
-                onClickJoin={()=>console.log("some Join Stuff")}/>
-            </div>
-            <div className={authContainer}>
-                <Auth />
-            </div>
-        </div>
-    );
+    if (display == true) {
+        return (
+            <LandingHeader
+                onClickHost={() => setDisplay(!display)}
+                onClickJoin={() => console.log("some Join Stuff")}
+            />
+        );
+    } else {
+        return <Auth />;
+    }
 };
 
 export default HomePage;
