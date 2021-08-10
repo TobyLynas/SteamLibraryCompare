fa7a85c05dddaa370899b417ce8e459b96b76189
Toby Lynas <tobylynas@tobys-air.lan>
2021-08-10 14:42:01 +0100

    Create front end Auth Page

--- client/components/FEAuth.tsx
+++ client/components/FEAuth.tsx
@@ -1,5 +1,30 @@
+import styles from "../styles/FEAuth.module.css";
+import Button from "../components/Button";
+
 const FEAuth = () => {
-    return <div>Something</div>;
+    return (
+        <div className={styles.main}>
+            <div className={styles.topRow}>
+                <Button
+                    className={styles.button}
+                    text="Sign in through Steam"
+                    onClick={() => console.log("Steam Auth Stuff")}
+                />
+                <div className={styles.or}>or</div>
+                <div className={styles.profileUrl}>
+                    Enter Your Steam profile URL:
+                    <input
+                        value="https://steamcommunity.com/profiles/..."
+                        className={styles.input}
+                    ></input>
+                </div>
+            </div>
+            <div className={styles.bottomRow}>
+                Please ensure your <b>game details is set to <i >public</i></b> so we can fetch
+                your game library.
+            </div>
+        </div>
+    );
 };
 
 export default FEAuth;
