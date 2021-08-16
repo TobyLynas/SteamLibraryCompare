b1671a7f81a0351222a58d56404025e20f1382d8
Toby Lynas <tobylynas@tobys-air.lan>
2021-08-16 12:22:36 +0100

    Add animations

--- client/src/components/FAQ.tsx
+++ client/src/components/FAQ.tsx
@@ -7,29 +7,34 @@ const FAQ = () => {
     const [FAQState, setState] = useState(false);
     return (
         <div className={styles.FAQBox}>
-            More questions?
+            Have more questions?
             <img
                 src={chevron}
                 alt="Downwards Chevron"
-                className={styles.chevron}
+                className={FAQState?styles.chevronRotate:styles.chevron}
                 onClick={() => setState(!FAQState)}
             />
-            {FAQState && (
-                <div className={styles.contentContainer}>
-                    <div className={styles.header}>
-                        <strong>
-                            Why do I need to sign into my Steam Account?
-                        </strong>
-                    </div>
-                    If you prefer to not sign in, you can alternatively enter
-                    your steam profile URL. We need your Steam ID in order to
-                    view your game library and
-                    <div className={styles.header}>
-                        <strong>Where can I send suggestions?</strong>
-                    </div>
-                    Somewhere else bro
+            {/* // <div className={styles.contentContainer}> */}
+            <div
+                className={
+                    FAQState
+                        ? styles.contentContainer
+                        : styles.contentContainerHide
+                }
+            >
+                <div className={styles.header}>
+                    <strong>
+                        Why do I need to sign into my Steam Account?
+                    </strong>
                 </div>
-            )}
+                If you prefer to not sign in, you can alternatively enter your
+                steam profile URL. We need your Steam ID in order to view your
+                game library and
+                <div className={styles.header}>
+                    <strong>Where can I send suggestions?</strong>
+                </div>
+                Somewhere else bro
+            </div>
         </div>
     );
 };
