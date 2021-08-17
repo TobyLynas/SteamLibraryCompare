477dec48b9087492eb46c722640cb7fc60a043a2
hensm <m@matt.tf>
2021-08-17 07:37:32 +0100

    Add button component size variants

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -3,12 +3,18 @@ import styles from "../styles/Button.module.css";
 
 interface ButtonProps {
     text: string;
+    variant?: string;
     onClick?: (ev: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = (props: ButtonProps) => {
+    let classNames = styles.button;
+    if (props.variant) {
+        classNames += ` ${styles[props.variant]}`;
+    }
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={classNames} onClick={props.onClick}>
             {props.text}
         </button>
     );
