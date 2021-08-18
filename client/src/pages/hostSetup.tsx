6d4bb1ced9f120b7be4878d4145ae85a0ec1be73
Toby Lynas <tobylynas@tobys-air.lan>
2021-08-18 15:09:09 +0100

    Polish CSS

--- client/src/pages/hostSetup.tsx
+++ client/src/pages/hostSetup.tsx
@@ -1,11 +1,22 @@
 import styles from "../styles/hostSetup.module.css";
 import Textbox from "../components/Textbox";
-
+import Checkbox from "../components/Checkbox";
+import Button from "../components/Button";
 const hostSetup = () => {
     return(
         <div className={styles.main}>
-            <h2>Host a Room</h2>
-            <Textbox placeholder="Hey there"/>
+            <div className={styles.title}>Host a Room</div>
+            <div className={styles.grid}>
+                <Textbox placeholder="Room Name" />
+                <Checkbox text="Only show games that everyone has" />
+                <div className={styles.enableAuth}>
+                    <div className={styles.padding}>
+                    <Checkbox text="Enable Authentication" />
+                    </div>
+                    <Textbox placeholder="Password"/>
+                </div>
+                <Button text="Create Room" variant="large" />
+            </div>
         </div>
     );
 };
