45701c9f0afb412c0f6a5c7a73f9f4c261d42abc
Toby Lynas <tobylynas@tobys-air.lan>
2021-08-20 11:17:57 +0100

    Refine Checkbox component

--- client/src/pages/hostSetup.tsx
+++ client/src/pages/hostSetup.tsx
@@ -7,13 +7,13 @@ const hostSetup = () => {
         <div className={styles.main}>
             <div className={styles.title}>Host a Room</div>
             <div className={styles.grid}>
-                <TextInput placeholder="Room Name" isPassword={false}/>
-                <Checkbox text="Only show games that everyone has" />
+                <TextInput placeholder="Room Name" isPassword={false} />
+                <Checkbox styling={{ alignSelf: "start"}} text="Only show games that everyone has" />
                 <div className={styles.enableAuth}>
                     <div className={styles.authSpacing}>
                         <Checkbox text="Enable Authentication" />
                     </div>
-                    <TextInput placeholder="Password" isPassword={true}/>
+                    <TextInput placeholder="Password" isPassword={true} />
                 </div>
                 <Button text="Create Room" variant="large" />
             </div>
