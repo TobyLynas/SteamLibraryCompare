ce8931611db6237a9c469b3d1d5bf980f78f547a
Toby Lynas <tobylynas@tobys-air.lan>
2021-08-20 10:46:06 +0100

    update naming of Textbox to TextInput

--- client/src/pages/hostSetup.tsx
+++ client/src/pages/hostSetup.tsx
@@ -1,6 +1,5 @@
 import styles from "../styles/hostSetup.module.css";
-import passwordStyle from "../styles/Textbox.module.css";
-import Textbox from "../components/Textbox";
+import TextInput from "../components/TextInput";
 import Checkbox from "../components/Checkbox";
 import Button from "../components/Button";
 const hostSetup = () => {
@@ -8,13 +7,13 @@ const hostSetup = () => {
         <div className={styles.main}>
             <div className={styles.title}>Host a Room</div>
             <div className={styles.grid}>
-                <Textbox placeholder="Room Name" />
+                <TextInput placeholder="Room Name" isPassword={false}/>
                 <Checkbox text="Only show games that everyone has" />
                 <div className={styles.enableAuth}>
                     <div className={styles.authSpacing}>
                         <Checkbox text="Enable Authentication" />
                     </div>
-                    <input className={passwordStyle.main} type="password" />
+                    <TextInput placeholder="Password" isPassword={true}/>
                 </div>
                 <Button text="Create Room" variant="large" />
             </div>
