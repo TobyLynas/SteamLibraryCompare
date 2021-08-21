1e3554c5c0d2164bf16691ec5adec55dad40903d
hensm <m@matt.tf>
2021-08-21 08:58:09 +0100

    Improve footer styling and disclaimer wording

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,27 +4,30 @@ import githubIcon from "../assets/github-icon.svg";
 const Footer = () => (
     <div className={styles.footer}>
         <div className={styles.links}>
-            <p>Find us on GitHub:</p>
             <a
-                style={{ marginTop: 5 }}
                 target="_blank"
                 rel="noreferrer"
                 href="https://github.com/TobyLynas/SteamLibaryCompare"
+                title="Find us on GitHub"
             >
                 <img
                     src={githubIcon}
                     alt="GitHub logo"
-                    width="25"
-                    height="25"
+                    width="36"
+                    height="36"
                 />
             </a>
         </div>
         <div className={styles.disclaimer}>
             <p>
-                This app is a community website and is not affiliated with Valve
-                or Steam.
+                {process.env.REACT_APP_NAME} is a community website and is not
+                affiliated with Valve or Steam.
             </p>
-            <p>Fair use disclaimer.</p>
+            <p className={styles.disclaimerCopyright}>
+                Steam and the Steam logo are trademarks and/or registered
+                trademarks of Valve Corporation in the U.S. and/or other
+                countries.
+            </p>
         </div>
     </div>
 );
