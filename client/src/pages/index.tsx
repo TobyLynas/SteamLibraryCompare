fc160374aa0562b5ed16f81f50655842d307c3a0
Toby Lynas <tobylynas@tobys-air.lan>
2021-09-16 16:25:25 +0100

    finalise styling

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -26,8 +26,18 @@ const Index = () => {
     return (
         <div className={styles.pageContainer}>
             <div className={styles.rearBox}></div>
-            <div className={styles.middleBox}></div>
-            <div className={styles.frontBox}></div>
+            <div className={styles.middleBox}>
+                <div className={styles.headingText}>
+                    Compare libraries with your freinds.
+                </div>
+                <div className={styles.bodyText}>
+                    we could put text here?
+                </div>
+            </div>
+            <div className={styles.frontBox}>
+                <img className={styles.imageContainer} src="sits_new3x.png" onClick={() => console.log("Do Something")} />
+                PLEASE ENSURE YOUR STEAM PROFILE IS PUBLIC
+            </div>
         </div>
     );
 };
