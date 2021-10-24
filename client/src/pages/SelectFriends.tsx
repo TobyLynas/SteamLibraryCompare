16df37118f852f9299a9fbcb658a1ae589c6135e
Toby Lynas <tobylynas@tobys-air.lan>
2021-10-24 13:26:11 +0100

    Add user specific content

--- client/src/pages/SelectFriends.tsx
+++ client/src/pages/SelectFriends.tsx
@@ -1,6 +1,31 @@
 import styles from "../styles/SelectFriends.module.css";
+import { useContext } from "react";
 import VotingPageHeader from "../components/VotingPageHeader";
+import UserContext from "../UserContext";
+
 const SelectFriends = () => {
+    const user = useContext(UserContext);
+    if (user) {
+   
+        return (
+            <div className={styles.page}>
+                <VotingPageHeader
+                    foreArrow="setup"
+                    aftArrow="Others"
+                    title="Choose Friends"
+                />
+                <div className={styles.container}>
+                    <div className={styles.primaryContent}>
+                        Select friends you want to compare libraries:
+                    </div>
+                    <div className={styles.secondaryContent}>
+                        {user.steamId}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className={styles.page}>
             <VotingPageHeader
