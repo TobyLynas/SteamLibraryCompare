77120a7e3780fbe5dcf93fed1ba5c04405d0e2f6
Toby Lynas <tobylynas@tobys-air.lan>
2022-01-24 22:29:54 +0000

    Commit for push

--- client/src/pages/SelectGames.tsx
+++ client/src/pages/SelectGames.tsx
@@ -1,7 +1,31 @@
+import Steam, { User } from "../lib/steam";
+import UserContext from "../UserContext";
+import { useContext, useEffect, useState } from "react";
+
+
 const SelectGames = () => {
+    const user = useContext(UserContext);
+    const [hostGamesArr, setHostGamesArr] = useState<User[]>();
+
+    useEffect(() => {
+        if(user) {
+            const steam = new Steam(user.token);
+
+            const getInfo = async () => {
+                const hostUser = await steam.getUserById(user.steamId);
+                const hostGames = 
+                    await hostUser.fetchGames();
+                console.log(hostGames);
+            };
+
+            getInfo();
+
+        }
+    })
     return (
         <div>
             Hello There
+            This is like intro text
         </div>
     )
 }
